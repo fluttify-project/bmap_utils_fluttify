946f758d21b8bdeef0f0b3635330d40cefaf7194
yohom <382146139@qq.com>
2020-06-04 14:39:13 +0800

    enhance: 导出

--- lib/bmap_utils_fluttify.dart
+++ lib/bmap_utils_fluttify.dart
@@ -1,13 +1,2 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class BmapUtilsFluttify {
-  static const MethodChannel _channel =
-      const MethodChannel('bmap_utils_fluttify');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'src/android/android.export.g.dart';
+export 'src/ios/ios.export.g.dart';
