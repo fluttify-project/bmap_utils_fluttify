76d22996b52a42d1fec1a190323cc21dfb8a1b90
yohom <382146139@qq.com>
2020-06-04 14:37:08 +0800

    roll engine #0967e1e7

---
+++ example/android/app/src/main/java/com/fluttify/bmap_utils_fluttify_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fluttify.bmap_utils_fluttify_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/src/android/android.export.g.dart
@@ -0,0 +1,3 @@
+export 'type_op.g.dart';
+export 'constants.g.dart';
+export 'package:bmap_core_fluttify/bmap_core_fluttify.dart';
